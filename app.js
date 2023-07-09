564661e56879d7f9e1fb67825c93737bb5032882
sabuz796 <sabuz796@gmail.com>
2023-07-09 23:55:32 +0200

    added hamburgerMenu while clicking

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 
 hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("ph-x");
   navList.classList.toggle("open");
   console.log("click");
 });
